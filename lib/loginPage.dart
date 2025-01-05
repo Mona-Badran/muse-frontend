c6e9928d2ef46a17945b8d5183a6e5eafec34878
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 04:10:12 +0200

    tested the button in login

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -16,7 +16,7 @@ class _LoginPageState extends State<LoginPage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: <Widget>[
-
+              Button("LOGIN"),
             ],
           ),
         ),
