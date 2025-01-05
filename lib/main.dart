390b93515f315d5cbef1c002faff597314d11a64
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 02:13:57 +0200

    moved the login class from main to the loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -24,17 +24,3 @@ class Muse extends StatelessWidget {
     );
   }
 }
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
