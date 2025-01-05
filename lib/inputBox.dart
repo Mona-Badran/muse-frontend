b62e30559557ace968bb964d22b38917e6127522
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 12:54:35 +0200

    added InputBox class

--- lib/inputBox.dart
+++ lib/inputBox.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class  extends StatelessWidget {
-  const ({super.key});
+class InputBox extends StatelessWidget {
+  const InputBox({super.key});
 
   @override
   Widget build(BuildContext context) {
