d1f536040e026db46fb7f7a8dded4c1344201460
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 19:36:54 +0200

    added statefull class

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 import 'button.dart';
 import 'inputBox.dart';
+
+class Registerpage extends StatefulWidget {
+  const Registerpage({super.key});
+
+  @override
+  State<Registerpage> createState() => _RegisterpageState();
+}
+
+class _RegisterpageState extends State<Registerpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
