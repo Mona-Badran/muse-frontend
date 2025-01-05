cf6155b120e2187bf08de987724337d04fa70f69
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 02:37:12 +0200

    prepared the outline for the login page

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
-
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -10,6 +9,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
   }
 }
