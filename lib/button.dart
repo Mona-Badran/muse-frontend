f272712f60356fce852aa48bb3c1007960ad307f
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 03:33:54 +0200

    added a stateful widget class called Button for all green buttons in the app

--- lib/button.dart
+++ lib/button.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+class Button extends StatefulWidget {
+  const Button({super.key});
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        print("Login");
+      },
+      child: Text("LOGIN"),
+    );
+  }
+}
