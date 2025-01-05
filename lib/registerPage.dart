626cd176d3b937e18d90f7d209baa9bbeb8d0dc4
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 19:39:33 +0200

    copied the login to the register

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -12,6 +12,59 @@ class Registerpage extends StatefulWidget {
 class _RegisterpageState extends State<Registerpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image(
+                image: AssetImage('assets/images/Vector.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image(
+                image: AssetImage('assets/images/Ellipse.png'),
+                fit: BoxFit.none,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  InputBox(
+                    hintText: 'Username',
+                    icon: Icons.person,
+                    obscureText: false,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InputBox(
+                    hintText: 'Password',
+                    icon: Icons.lock,
+                    obscureText: false,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Button("LOGIN"),
+                  SizedBox(
+                    height: 40,
+                    child: Image(
+                      image: AssetImage('assets/images/Ellipse 1.png'),
+                    ),
+                  ),
+                  Button("GET REGISTER"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
