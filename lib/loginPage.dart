8518860b040fabcfdb7a2dbdce26f94498cf093d
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-05 19:37:55 +0200

    added background images

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -13,37 +13,55 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              InputBox(
-                hintText: 'Username',
-                icon: Icons.person,
-                obscureText : false,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image(
+                image: AssetImage('assets/images/Vector.png'),
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: 20,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image(
+                image: AssetImage('assets/images/Ellipse.png'),
+                fit: BoxFit.none,
               ),
-              InputBox(
-                hintText: 'Password',
-                icon: Icons.lock,
-                obscureText : false,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  InputBox(
+                    hintText: 'Username',
+                    icon: Icons.person,
+                    obscureText: false,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InputBox(
+                    hintText: 'Password',
+                    icon: Icons.lock,
+                    obscureText: false,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Button("LOGIN"),
+                  SizedBox(
+                    height: 40,
+                    child: Image(
+                      image: AssetImage('assets/images/Ellipse 1.png'),
+                    ),
+                  ),
+                  Button("GET REGISTER"),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Button("LOGIN"),
-              SizedBox(
-                height: 40,
-                child: Image(
-                  image: AssetImage('assets/images/Ellipse 1.png'),
-                ),
-              ),
-              Button("GET REGISTER"),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
