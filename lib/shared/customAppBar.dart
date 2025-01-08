8dfa34d7c6c2c511a1c80d2296956600094da421
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 00:24:18 +0200

    added a props and const for the CustomAppBar

--- lib/shared/customAppBar.dart
+++ lib/shared/customAppBar.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class CustomAppBar  extends StatelessWidget {
   const CustomAppBar ({super.key});
 
+  final String title;
+  final VoidCallback onAvatarTap;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
