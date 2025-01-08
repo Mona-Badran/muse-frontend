712fd157107f6ecf75c291b006806151ee8fc8fc
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 01:41:54 +0200

    added placeholder ProfilePage

--- lib/shared/bottomNavBar.dart
+++ lib/shared/bottomNavBar.dart
@@ -45,3 +45,10 @@ class SettingsPage extends StatelessWidget {
     return Center(child: Text("Settings Page"));
   }
 }
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Profile Page"));
+  }
+}
