2c358c406fe217cbd11f50c20c7c7b97e5a160fa
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-08 22:09:50 +0200

    imported register page

--- lib/features/auth/loginPage.dart
+++ lib/features/auth/loginPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../shared/button.dart';
 import '../../shared/inputBox.dart';
+import 'registerPage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
