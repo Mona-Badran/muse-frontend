47a352dfcb11eea85281f3bf7e24e1dc158c010e
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-08 23:28:44 +0200

    added a props and const for the ArchImage

--- lib/features/galleries/allGalleries.dart
+++ lib/features/galleries/allGalleries.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ArchImage extends StatelessWidget {
-  const ArchImage({super.key});
+  final String imagePath;
+  final double width;
+  final double height;
+
+  const ArchImage({
+    required this.imagePath,
+    required this.width,
+    required this.height,
+  });
 
   @override
   Widget build(BuildContext context) {
