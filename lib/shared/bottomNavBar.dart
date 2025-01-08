70c75a6f627d61afc7c2af6910b456eca25fd344
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 01:32:41 +0200

    added a Stateful class for the bottom Nav Bar

--- lib/shared/bottomNavBar.dart
+++ lib/shared/bottomNavBar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
+
