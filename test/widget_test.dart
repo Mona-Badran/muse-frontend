f7925c81cf3d3e131a4cea3e202a0bbd9c5786b1
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-08 21:54:10 +0200

    edited the test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:muse_frontend/main.dart';
+import 'package:muse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
