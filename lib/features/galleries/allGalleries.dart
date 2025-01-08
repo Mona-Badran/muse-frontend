83f59c7a16b6a8682061a3c9e307e7c0660e0d89
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 01:39:30 +0200

    added the class for the galleries

---
+++ lib/features/galleries/allGalleries.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllGalleries extends StatefulWidget {
+  const AllGalleries({super.key});
+
+  @override
+  State<AllGalleries> createState() => _AllGalleriesState();
+}
+
+class _AllGalleriesState extends State<AllGalleries> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
