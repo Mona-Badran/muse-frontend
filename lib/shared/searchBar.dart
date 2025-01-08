ff117d26ccc10ade427b8edc011b3ffa71ede803
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 01:21:39 +0200

    added a the border for the SearchBar

--- lib/shared/searchBar.dart
+++ lib/shared/searchBar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
-
   final TextEditingController controller;
   final ValueChanged<String> onChanged;
   final String hintText;
@@ -27,6 +26,13 @@ class SearchBar extends StatelessWidget {
           color: Colors.grey[600],
         ),
         border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
           borderSide: BorderSide(
             color: Colors.grey,
