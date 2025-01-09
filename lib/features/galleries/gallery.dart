54d1f9cd9e7a2fdeac3b45d112fd01a684b3a4af
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 20:00:02 +0200

    added Gallery Details Page class with props and const

--- lib/features/galleries/gallery.dart
+++ lib/features/galleries/gallery.dart
@@ -3,6 +3,27 @@ import '../../shared/archImage.dart';
 import '../../shared/customAppBar.dart';
 import '../../shared/customButton.dart';
 
+class GalleryDetailsPage extends StatelessWidget {
+  final String name;
+  final String city;
+  final String country;
+  final String imagePath;
+
+  const GalleryDetailsPage({
+    Key? key,
+    required this.name,
+    required this.city,
+    required this.country,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
 class Events extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
