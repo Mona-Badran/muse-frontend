c778c71fefc64dbff8fd86cd2e696fabfaaa257d
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 10:47:24 +0200

    rearranged lines of code

--- lib/shared/customBottomNavBar.dart
+++ lib/shared/customBottomNavBar.dart
@@ -23,12 +23,12 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
     return Scaffold(
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
           onTap: (index) {
             setState(() {
               _currentIndex = index;
             });
           },
-          currentIndex: _currentIndex,
           type: BottomNavigationBarType.fixed,
           backgroundColor: Color(0xFF749495),
           unselectedItemColor: Colors.white70,
