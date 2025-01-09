fd2d52a61c61abe7c3438e79edf9b6a6c315aff4
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 20:11:57 +0200

    added class EditProfilePage

--- lib/features/dashboard/profile.dart
+++ lib/features/dashboard/profile.dart
@@ -3,3 +3,14 @@ import 'package:muse/shared/customBottomNavBar.dart';
 import '../galleries/allGalleries.dart';
 import '../../shared/customAppBar.dart';
 import '../../shared/customButton.dart';
+
+class EditProfilePage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
