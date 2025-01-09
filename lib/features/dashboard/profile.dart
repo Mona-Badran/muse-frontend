d520ddf051257273644c8c2978e719ea09dff824
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 20:09:49 +0200

    imported necessary files

---
+++ lib/features/dashboard/profile.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:muse/shared/customBottomNavBar.dart';
+import '../galleries/allGalleries.dart';
+import '../../shared/customAppBar.dart';
+import '../../shared/customButton.dart';
