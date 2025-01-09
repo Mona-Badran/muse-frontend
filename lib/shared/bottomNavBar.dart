d7624b8a18f2593fffa716203823b9673c295584
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 02:20:02 +0200

    displayed the Icons for library

--- lib/shared/bottomNavBar.dart
+++ lib/shared/bottomNavBar.dart
@@ -9,7 +9,6 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-
   int _currentIndex = 0;
   final List<Widget> _pages = [
     AllGalleries(),
@@ -23,6 +22,14 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          items: const[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo_library),
+              label: 'Galleries',
+            ),
+          ]
+      ),
     );
   }
 }
