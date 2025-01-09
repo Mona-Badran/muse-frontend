bcdcbc9e000417185ae97602ec06deca5d524f05
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-09 19:58:32 +0200

    added mock event class

--- lib/features/galleries/gallery.dart
+++ lib/features/galleries/gallery.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 import '../../shared/archImage.dart';
 import '../../shared/customAppBar.dart';
 import '../../shared/customButton.dart';
+
+class Events extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Events Page"));
+  }
+}
