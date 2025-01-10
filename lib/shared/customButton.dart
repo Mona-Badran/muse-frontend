d5b61cb5b59d3b46ef7d2f1dff70a78ea9899ea7
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-10 02:19:15 +0200

    edited the button

--- lib/shared/customButton.dart
+++ lib/shared/customButton.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import '../features/auth/loginPage.dart';
 
 class Button extends StatefulWidget {
   final String buttonText;
   final VoidCallback? onPressed;
-  const Button(
-      this.buttonText,
-      this.onPressed
-      );
+  const Button(this.buttonText, {this.onPressed});
 
   @override
   State<Button> createState() => _ButtonState();
@@ -17,7 +13,7 @@ class _ButtonState extends State<Button> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed:  widget.onPressed,
+      onPressed: widget.onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: Color(0xFF789495), // Background color
         padding: EdgeInsets.symmetric(vertical: 16),
