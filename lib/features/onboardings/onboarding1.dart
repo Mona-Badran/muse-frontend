09cd6f70b3f043ce5704476acf22785946f52f0c
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-14 14:40:36 +0200

    added Stack

--- lib/features/onboardings/onboarding1.dart
+++ lib/features/onboardings/onboarding1.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Onboarding1 extends StatelessWidget {
+  const Onboarding1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: <Widget>[],
+      )),
+    );
+  }
+}
