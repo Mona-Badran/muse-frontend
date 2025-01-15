d04cf87f01fd125f2ce813cd4e7bd69e77bd3c4d
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-15 02:32:57 +0200

    added class onboarding 3

--- lib/features/onboardings/onboarding3.dart
+++ lib/features/onboardings/onboarding3.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Onboarding3 extends StatelessWidget {
+  const Onboarding3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
