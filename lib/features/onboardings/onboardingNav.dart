0bcab689177ec79870feb941edca63887add4164
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-15 03:29:53 +0200

    changed the list of pages to a map

--- lib/features/onboardings/onboardingNav.dart
+++ lib/features/onboardings/onboardingNav.dart
@@ -12,15 +12,57 @@ class OnboardingNav extends StatefulWidget {
 
 class _OnboardingNavState extends State<OnboardingNav> {
   int _currentIndex = 0;
-  final List<Widget> _pages = [
-    Onboarding1(),
-    Onboarding2(),
-    Onboarding3(),
+  final List<Map<String, dynamic>> _pages = [
+    {
+      'page': Onboarding1(),
+      'text': 'Explore the world of art and culture',
+      'buttonAction': () => null, // Add custom actions here
+    },
+    {
+      'page': Onboarding2(),
+      'text': 'Discover art that catches your eye',
+      'buttonAction': () => null,
+    },
+    {
+      'page': Onboarding3(),
+      'text': 'Visit online Galleries',
+      'buttonAction': () => null,
+    },
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_currentIndex],
+      bottomSheet: Container(
+        height: 250,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Color(0xFF789495),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(60),
+            topRight: Radius.circular(60),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Explore the world of art and culture',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
