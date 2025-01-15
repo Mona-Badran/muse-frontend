010eb549ab115574661144ccfd11fa69751dc9a8
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-15 02:25:43 +0200

    created class OnboardingNav

--- lib/features/onboardings/onboardingNav.dart
+++ lib/features/onboardings/onboardingNav.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class OnboardingNav extends StatefulWidget {
+  const OnboardingNav({super.key});
+
+  @override
+  State<OnboardingNav> createState() => _OnboardingNavState();
+}
+
+class _OnboardingNavState extends State<OnboardingNav> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
