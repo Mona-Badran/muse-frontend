d02e206c3d57a830262d79121f1842fe71580bde
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-15 13:56:10 +0200

    fixed the page

--- lib/features/onboardings/onboarding1.dart
+++ lib/features/onboardings/onboarding1.dart
@@ -5,65 +5,31 @@ class Onboarding1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/background_shape.png',
-              fit: BoxFit.cover,
-            ),
+    return Stack(
+      children: <Widget>[
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/background_shape.png',
+            fit: BoxFit.cover,
           ),
-          Positioned(
-            top: 70,
-            right: 0,
-            child: Image.asset(
-              'assets/images/tower.png',
-              width: 200,
-            ),
+        ),
+        Positioned(
+          top: 70,
+          right: 0,
+          child: Image.asset(
+            'assets/images/tower.png',
+            width: 200,
           ),
-          Positioned(
-            bottom: 150,
-            left: 40,
-            child: Image.asset(
-              'assets/images/girl.png',
-              width: 120,
-            ),
+        ),
+        Positioned(
+          bottom: 150,
+          left: 40,
+          child: Image.asset(
+            'assets/images/girl.png',
+            width: 120,
           ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              height: 250,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Color(0xFF789495),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(60),
-                  topRight: Radius.circular(60),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Explore the world of art and culture',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.normal,
-                        fontFamily: 'Poppins',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
