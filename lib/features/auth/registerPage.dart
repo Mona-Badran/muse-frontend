e2c2b1de4c767d02cac46b522e69a1565d038cc4
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-18 02:10:34 +0200

    changed location to description

--- lib/features/auth/registerPage.dart
+++ lib/features/auth/registerPage.dart
@@ -19,13 +19,13 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController galleryNameController = TextEditingController();
-  final TextEditingController locationController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
 
   void handleRegister() async {
     final username = usernameController.text;
     final password = passwordController.text;
     final galleryName = isChecked ? galleryNameController.text : null;
-    final location = isChecked ? locationController.text : null;
+    final description = isChecked ? descriptionController.text : null;
 
     if (username.isEmpty || password.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -46,7 +46,7 @@ class _RegisterPageState extends State<RegisterPage> {
           'username': username,
           'password': password,
           if (isChecked) 'galleryName': galleryName,
-          if (isChecked) 'location': location,
+          if (isChecked) 'description': description,
         }),
       );
       if (response.statusCode == 201) {
@@ -83,128 +83,130 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Image(
-                image: AssetImage('assets/images/Vector.png'),
-                fit: BoxFit.cover,
+        // child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image(
+                  image: AssetImage('assets/images/Vector.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              child: Image(
-                image: AssetImage('assets/images/Ellipse.png'),
-                fit: BoxFit.none,
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: Image(
+                  image: AssetImage('assets/images/Ellipse.png'),
+                  fit: BoxFit.none,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.transparent,
-                    child: Image(
-                      image: AssetImage('assets/images/musec.png'),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Register",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  InputBox(
-                    hintText: 'Username',
-                    icon: Icons.person_2_outlined,
-                    obscureText: false,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  InputBox(
-                    hintText: 'Password',
-                    icon: Icons.lock_outline,
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Checkbox(
-                        value: isChecked,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            isChecked = value ?? false;
-                          });
-                        },
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.transparent,
+                      child: Image(
+                        image: AssetImage('assets/images/musec.png'),
                       ),
-                      Text(
-                        "Gallery Owner",
-                        style: TextStyle(
-                          fontSize: 16, // Adjust font size
-                          fontFamily: 'Poppins', // Use your custom font
-                          color: Colors.black, // Text color
-                        ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                  if (isChecked) ...[
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputBox(
+                      hintText: 'Username',
+                      icon: Icons.person_2_outlined,
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputBox(
+                      hintText: 'Password',
+                      icon: Icons.lock_outline,
+                      obscureText: true,
+                    ),
                     SizedBox(
                       height: 10,
                     ),
-                    InputBox(
-                      hintText: 'Gallery Name',
-                      icon: Icons.image_outlined,
-                      obscureText: false,
+                    Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              isChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        Text(
+                          "Gallery Owner",
+                          style: TextStyle(
+                            fontSize: 16, // Adjust font size
+                            fontFamily: 'Poppins', // Use your custom font
+                            color: Colors.black, // Text color
+                          ),
+                        ),
+                      ],
+                    ),
+                    if (isChecked) ...[
+                      SizedBox(
+                        height: 10,
+                      ),
+                      InputBox(
+                        hintText: 'Gallery Name',
+                        icon: Icons.image_outlined,
+                        obscureText: false,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      InputBox(
+                        hintText: 'Description',
+                        icon: Icons.description_outlined,
+                        obscureText: false,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                    Button(
+                      "REGISTER",
+                      onPressed: handleRegister,
                     ),
                     SizedBox(
-                      height: 20,
+                      height: 40,
+                      child: Image(
+                        image: AssetImage('assets/images/Ellipse 1.png'),
+                      ),
                     ),
-                    InputBox(
-                      hintText: 'Location',
-                      icon: Icons.location_on_outlined,
-                      obscureText: false,
-                    ),
-                    SizedBox(
-                      height: 20,
+                    Button(
+                      "BACK TO LOGIN",
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
                     ),
                   ],
-                  Button(
-                    "REGISTER",
-                    onPressed: handleRegister,
-                  ),
-                  SizedBox(
-                    height: 40,
-                    child: Image(
-                      image: AssetImage('assets/images/Ellipse 1.png'),
-                    ),
-                  ),
-                  Button(
-                    "BACK TO LOGIN",
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        // ),
       ),
     );
   }
