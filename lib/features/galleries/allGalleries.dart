066da9d2e3d645f5d75828700a5b016992df1f81
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-19 02:10:26 +0200

    pasted back all the code

--- lib/features/galleries/allGalleries.dart
+++ lib/features/galleries/allGalleries.dart
@@ -17,7 +17,76 @@ class AllGalleries extends StatefulWidget {
 class _AllGalleriesState extends State<AllGalleries> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        CustomAppBar(
+          title: 'Galleries',
+          onAvatarTap: () {
+            print('Avatar tapped');
+          },
+          avatarUrl: null,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: CustomSearchBar(
+            controller: searchController,
+            onChanged: (query) {
+              print('Search query: $query');
+            },
+            hintText: "Search for galleries",
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20.0,
+                mainAxisSpacing: 10.0,
+              ),
+              itemCount: galleries.length,
+              itemBuilder: (context, index) {
+                final gallery = galleries[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GalleryDetailsPage(
+                          name: gallery.name,
+                          city: gallery.city,
+                          country: gallery.country,
+                          imagePath: gallery.imagePath,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible (
+                        child: ArchImage(
+                          imagePath: gallery.imagePath,
+                        ),
+                      ),
+                      Text(
+                        gallery.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
