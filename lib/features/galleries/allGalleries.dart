8aacd421fd8d645c481f7548df379349c43d788c
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-19 02:09:57 +0200

    deleted the stateless class an placed a stateful one

--- lib/features/galleries/allGalleries.dart
+++ lib/features/galleries/allGalleries.dart
@@ -7,80 +7,17 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../config.dart';
 
-class AllGalleries extends StatelessWidget {
-  final TextEditingController searchController = TextEditingController();
+class AllGalleries extends StatefulWidget {
+  const AllGalleries({super.key});
 
   @override
+  State<AllGalleries> createState() => _AllGalleriesState();
+}
+
+class _AllGalleriesState extends State<AllGalleries> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(
-          title: 'Galleries',
-          onAvatarTap: () {
-            print('Avatar tapped');
-          },
-          avatarUrl: null,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: CustomSearchBar(
-            controller: searchController,
-            onChanged: (query) {
-              print('Search query: $query');
-            },
-            hintText: "Search for galleries",
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 20.0,
-                mainAxisSpacing: 10.0,
-              ),
-              itemCount: galleries.length,
-              itemBuilder: (context, index) {
-                final gallery = galleries[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => GalleryDetailsPage(
-                          name: gallery.name,
-                          city: gallery.city,
-                          country: gallery.country,
-                          imagePath: gallery.imagePath,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Flexible (
-                        child: ArchImage(
-                          imagePath: gallery.imagePath,
-                        ),
-                      ),
-                      Text(
-                        gallery.name,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Poppins',
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ],
-    );
+    return const Placeholder();
   }
 }
+
