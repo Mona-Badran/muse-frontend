de2262d9c2452279c869f7903cc72dd2aae7ffeb
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 06:09:05 +0200

    imported http from backend

--- lib/features/artwork/cameraPage.dart
+++ lib/features/artwork/cameraPage.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
