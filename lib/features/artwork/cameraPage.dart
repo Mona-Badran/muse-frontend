fd608f2239bf1a6a58253adedc7e5692dbafb11b
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-22 00:40:37 +0200

    imported few libraries

--- lib/features/artwork/cameraPage.dart
+++ lib/features/artwork/cameraPage.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'package:muse/features/artwork/resultPage.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../config.dart';
 
