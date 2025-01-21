4ebe52ab39db9aaf0779e3618085a299db986d69
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 22:53:18 +0200

    deleted camera page code and replaced it

--- lib/features/artwork/cameraPage.dart
+++ lib/features/artwork/cameraPage.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
-import '../../config.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -13,19 +11,16 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   File? _selectedImage;
-  bool _isUploading = false;
-
-  // Placeholder user ID
-  final int _userId = 1;
 
   @override
   void initState() {
     super.initState();
-    _captureImage();
+    _pickImageFromCamera();
   }
 
-  Future _captureImage() async {
-    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+  Future _pickImageFromCamera() async {
+    final returnedImage =
+    await ImagePicker().pickImage(source: ImageSource.camera);
 
     if (returnedImage == null) {
       return;
@@ -33,55 +28,6 @@ class _CameraPageState extends State<CameraPage> {
     setState(() {
       _selectedImage = File(returnedImage.path);
     });
-    await _uploadImageToServer();
-  }
-
-  Future<void> _uploadImageToServer() async {
-    if (_selectedImage == null) return;
-
-    setState(() {
-      _isUploading = true;
-    });
-    try {
-      final url = Uri.parse('$BASE_URL/artwork/upload');
-      var request = http.MultipartRequest('POST', url);
-      request.files.add(await http.MultipartFile.fromPath(
-          'image',
-          _selectedImage!.path,
-      ));
-      request.fields['user_id'] = _userId.toString();
-
-      var response = await request.send();
-
-      if (response.statusCode == 201) {
-        final responseData = await response.stream.bytesToString();
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text("Image uploaded successfully!"),
-            backgroundColor: Colors.green,
-          ),
-        );
-      }
-      else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Upload failed: ${response.statusCode}"),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Error uploading image: $error"),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }finally {
-      setState(() {
-        _isUploading = false;
-      });
-    }
   }
 
   @override
