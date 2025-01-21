f15c85f1ff7c20afe0d0ea68c119c435ad822195
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2025-01-21 06:22:55 +0200

    added response

--- lib/features/artwork/cameraPage.dart
+++ lib/features/artwork/cameraPage.dart
@@ -43,6 +43,9 @@ class _CameraPageState extends State<CameraPage> {
       final url = Uri.parse('$BASE_URL/artwork/upload');
       var request = http.MultipartRequest('POST', url);
       request.files.add(await http.MultipartFile.fromPath('image', _selectedImage!.path));
+
+      var response = await request.send();
+
     } catch (error) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
